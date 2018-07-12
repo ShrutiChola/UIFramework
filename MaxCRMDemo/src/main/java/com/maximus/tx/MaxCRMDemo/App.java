8371ef4a06c75c82609545a71ae5135688878125
200089 <200089@NDFMM-BNTJPN2.MAXCORP.MAXIMUS>
2018-07-12 13:05:23 -0500

    Added Object Repo , Page Lib , Test Base to package structure

---
+++ MaxCRMDemo/src/main/java/com/maximus/tx/MaxCRMDemo/App.java
@@ -0,0 +1,13 @@
+package com.maximus.tx.MaxCRMDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
